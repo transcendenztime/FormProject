92a4170ab56866b3ae341a291d7423bd1715e246
vadim.strelets <transcendenztime@gmail.com>
2019-07-23 15:03:56 +0300

    first commit

---
+++ App.js
@@ -0,0 +1,14 @@
+﻿"use strict";
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import ExampleComponent from './components/ExampleComponent';
+
+// если необходимо, вид сборки можно проверить в коде:
+// if (process.env.NODE_ENV === 'production') {
+// if (process.env.NODE_ENV !== 'production') {
+
+ReactDOM.render( 
+  <ExampleComponent />
+, document.getElementById('container') );
